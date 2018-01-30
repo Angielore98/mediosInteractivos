2c28b71d502f06a0acda520ae97f10ec9b978e2c
Angielore98 <35973393+Angielore98@users.noreply.github.com>
2018-01-30 14:46:16 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,70 @@
+function setup() {
+createCanvas(310,400) //Tamaño del papel
+background(246,240,222); // Color papel
+	strokeWeight(0); // Tamaño pincel
+	fill(204,113,97); // Color del relleno
+	ellipse(78,78,150,150);// Dibujar un e,lipse
+	fill(246,195,79,150); // Color del relleno
+	ellipse(115,135,60,60);
+	fill(206,156,239,); // Color del relleno
+	ellipse(115,135,50,50); // Dibujar una elipse
+	fill(0,0,0); // Color del relleno
+	ellipse(78,78,108,108);
+	fill(70,37,99); // Color del relleno
+	ellipse(78,78,55,55); // Dibujar una elipse
+	fill(101,165,206); // Color del relleno
+	ellipse(65,285,70,70); // Dibujar una elipse
+	fill(246,195,79); // Color del relleno
+	ellipse(65,285,60,60);
+	fill(0,0,255); // Color del relleno
+	ellipse(65,285,50,50); // Dibujar una elipse
+		fill(246,195,79); // Color del relleno
+	ellipse(230,350,70,70); // Dibujar una elipse
+	fill(230,188,158); // Color del relleno
+	ellipse(225,345,50,50);
+	fill(101,165,206); // Color del relleno
+	ellipse(220,340,40,40); // Dibujar una elipse
+	fill(255,255,255); // Color del relleno
+	ellipse(290,120,70,70); // Dibujar una elipse
+	fill(0,0,255); // Color del relleno
+	ellipse(290,120,50,50);
+	fill(255,255,255); // Color del relleno
+	ellipse(290,120,40,40); // Dibujar una elipse
+stroke(0,0,0);
+fill(101,165,206,150);
+beginShape(); // Iniciar una figura
+vertex(310,200); // Crear un vertice
+vertex(195,100);
+vertex(109,192);
+endShape(); // Cerrar una figura
+strokeWeight(2); // Tamaño pincel
+stroke(0); // Color pincel
+line(194,140,192,38); // Dibujar linea
+strokeWeight(3); // Tamaño pincel
+stroke(0,0,0,); // Color pincel
+line(180,80,220,80); // Dibujar linea
+	strokeWeight(2); // Tamaño pincel
+stroke(0,0,0,100); // Color pincel
+line(175,72,212,72); // Dibujar linea
+	strokeWeight(2); // Tamaño pincel
+stroke(0,0,0,); // Color pincel
+line(170,40,240,72); // Dibujar linea
+	stroke(0,0,0);
+fill(101,165,206,150);
+beginShape(); // Iniciar una figura
+vertex(170,300); // Crear un vertice
+vertex(180,292);
+vertex(82,243);
+	vertex(80,248);
+endShape(); // Cerrar una figura
+	strokeWeight(10); // Tamaño pincel
+stroke(255,255,0); // Color de la linea
+fill(0,0,255); // Color del relleno
+	strokeWeight(10); // Tamaño pincel
+	stroke(0,0,0,); // Color pincel
+  fill(0, 0, 0,0);
+	curve(220, 90, 210,110, 190, 130,160,130)
+ arc(215,90,160,130,0, PI+HALF_PI, OPEN);
+arc(5, 25, 190, 130, 0, PI+QUARTER_PI, OPEN);
+
+} 
