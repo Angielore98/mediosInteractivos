aff82ec12ea9e631151eeeaa34388218b4d1fcf4
Angielore98 <35973393+Angielore98@users.noreply.github.com>
2018-03-20 14:05:49 -0500

    Add files via upload

---
+++ 08/p5js-temp-sketch_180319hablar3115754834679756844.js
@@ -0,0 +1,65 @@
+var mic;
+var miS;
+var amplitud;
+var tempo;
+
+function preload(){
+  sound = loadSound('musica/ht1.mp3');
+}
+function setup(){
+  createCanvas(windowWidth,windowHeight)
+  mic = new p5.AudioIn()
+  mic.start();
+ amplitud = new p5.Amplitude();
+ tempo = millis();
+  var cnv = createCanvas(windowWidth,windowHeight);
+cnv.mouseClicked(togglePlay);
+  fft = new p5.FFT();
+  sound.amp(0.2);
+  start=millis()
+ 
+}
+function draw(){
+  
+ fill(0,200,mouseX+10)
+  micLevel = mic.getLevel();
+  ellipse((width/2)+(width/3), constrain(height/2-micLevel*height/2*5, 0, height/2), constrain(height/2-micLevel*height/2*5, 0, height/2), 10);
+  
+  
+  fill(0)
+  rect(0,400,width,height)
+  
+  var spectrum = fft.analyze();
+  noStroke();
+  fill(255,0,0); // spectrum es color rojo
+  for (var i = 0; i< spectrum.length; i++){
+    var x = map(i, 0, spectrum.length, 0, width);
+    var h = -height/4 + map(spectrum[i], 0, 255, height/4, 0);
+    rect(x, (height/2)+300, width / spectrum.length, h )
+  }
+
+  var waveform = fft.waveform();
+
+  noFill();
+  beginShape();
+  stroke(0,0,255); // waveform es azul
+  strokeWeight(1);
+  for (var i = 0; i< waveform.length; i++){
+    var x = map(i, 0, waveform.length, 0, width);
+    var y = map( waveform[i], -1, 1, height/2+(height/3), height);
+    vertex(x,y);
+  }
+  endShape();
+
+  text('click to play/pause', 4, 10);
+}
+
+//se pausa el sonido si se hace clic sobre el canvas
+function togglePlay() {
+  if (sound.isPlaying()) {
+    sound.pause();
+  } else {
+    sound.loop();
+  }
+
+}
